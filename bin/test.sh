4eb3433707822e2d4bcb8c124af19bd29c031d93
yepengxj <yepeng_xj@163.com>
2015-11-05 20:52:12 +0800

    modified:   test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,1 +1,1 @@
-echo "===pwd===" && pwd && env && ls -a ./.conda/bin/*
+echo "===pwd===" && pwd && env && ls -a ./.conda/bin/* && ./.conda/bin/ipython profile create && ./.conda/bin/ipython notebook --port=$VCAP_APP_PORT --ip=$VCAP_APP_HOST --no-mathjax
