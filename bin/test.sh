46accee292b1f5675d9324c67d82d1421384a1fd
yepengxj <yepeng_xj@163.com>
2015-11-06 11:03:13 +0800

    modified:   test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,1 +1,1 @@
-echo "===pwd===" && pwd && env && echo "create profile" && ./.conda/bin/ipython profile create && echo "ipython notebook" && ./.conda/bin/ipython notebook --port=$PORT --ip=0.0.0.0 --no-mathjax
+echo "===pwd===" && pwd && env && echo "create profile" && ./.conda/bin/ipython profile create && echo "jupyter notebook" && ./.conda/bin/ipython notebook --port=$PORT --ip=0.0.0.0 --no-mathjax
