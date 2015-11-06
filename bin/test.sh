7546c80a37da21cd50aea897d9bce13a53ded1df
yepengxj <yepeng_xj@163.com>
2015-11-06 11:34:01 +0800

    modified:   compile 	deleted:    test.sh

--- bin/test.sh
+++
@@ -1,1 +0,0 @@
-echo "===pwd===" && pwd && env && echo "create profile" && ./.conda/bin/ipython profile create && echo "jupyter notebook" && ./.conda/bin/ipython notebook --port=$PORT --ip=0.0.0.0 --no-mathjax
